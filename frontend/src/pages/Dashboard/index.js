8ecf01686baac1d3dc833525930755a034d736a0
unknown <hellowchatfull@gmail.com>
2022-05-06 23:50:02 -0300

    relatorio front

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,17 +1,17 @@
 import React, { useContext } from "react"
-
+import { useState, useEffect } from "react";
 import Paper from "@material-ui/core/Paper"
 import Container from "@material-ui/core/Container"
 import Grid from "@material-ui/core/Grid"
 import { makeStyles } from "@material-ui/core/styles"
 import Typography from "@material-ui/core/Typography";
-
-import useTickets from "../../hooks/useTickets"
-
+import Button from '@material-ui/core/Button';
+import SearchFull from "../../hooks/useTickets/filter"
+import TextField from '@material-ui/core/TextField';
 import { AuthContext } from "../../context/Auth/AuthContext";
-
+import Select from '@material-ui/core/Select';
 import { i18n } from "../../translate/i18n";
-
+import MenuItem from '@material-ui/core/MenuItem';
 import Chart from "./Chart"
 
 const useStyles = makeStyles(theme => ({
@@ -47,29 +47,122 @@ const useStyles = makeStyles(theme => ({
 }))
 
 const Dashboard = () => {
+
 	const classes = useStyles()
 
+
 	const { user } = useContext(AuthContext);
-	var userQueueIds = [];
 
-	if (user.queues && user.queues.length > 0) {
-		userQueueIds = user.queues.map(q => q.id);
-	}
 
-	const GetTickets = (status, showAll, withUnreadMessages) => {
 
-		const { count } = useTickets({
-			status: status,
-			showAll: showAll,
-			withUnreadMessages: withUnreadMessages,
-			queueIds: JSON.stringify(userQueueIds)
+
+	const [userId, setUserId] = useState(0);
+	const [dateIni, setDateIni] = useState('');
+	const [dateFin, setDateFin] = useState('');
+
+
+
+	const [openTickets, setOpenTickets] = useState([]);
+	const [pendingTickets, setPendingTickets] = useState([]);
+	const [closedTickets, setClosedTickets] = useState([]);
+
+
+	const SearchTickets = (userId, dateIni, dateFin, status) => {
+
+		//passar datainicial
+		//passar datafinal
+		//passar atendente
+
+		const { tickets } = SearchFull({
+			userId: userId,
+			dateIni: dateIni,
+			dateFin: dateFin,
+			status: status
 		});
-		return count;
+		return tickets;
 	}
+	const buttonSearchByUserId = () => {
+/* 
+		const ticketsOpen = this.SearchTickets(userId, dateIni, dateFin, 'open');
+
+		const ticketsPending = this.SearchTickets(userId, dateIni, dateFin, 'pending');
+		const ticketsClosed = this.SearchTickets(userId, dateIni, dateFin, 'closed');
+
+		
+		setOpenTickets([ticketsOpen])
+		pendingTickets([ticketsPending])
+		closedTickets([ticketsClosed]) */
+		/* 	setOpenTickets(SearchTickets(userId, dateIni, dateFin, 'open'))
+			setPendingTickets(SearchTickets(userId, dateIni, dateFin, 'pending'))
+			setClosedTickets(SearchTickets(userId, dateIni, dateFin, 'closed')) */
+	}
+
+	useEffect(()=> {
+		
+		/* const ticketsOpen = this.SearchTickets(userId, dateIni, dateFin, 'open');
+
+		const ticketsPending = this.SearchTickets(userId, dateIni, dateFin, 'pending');
+		const ticketsClosed = this.SearchTickets(userId, dateIni, dateFin, 'closed');
+ */
+		
+		setOpenTickets([10])
+		pendingTickets([40])
+		closedTickets([100])
+	}, []);
 
 	return (
 		<div>
 			<Container maxWidth="lg" className={classes.container}>
+
+
+				<form className={classes.container} noValidate>
+					<Select
+						value={userId}
+						onChange={
+							(e) => setUserId(e.target.value)}
+						style={{ width: '150px', marginRight: '10px' }}
+
+						name="searchUserId"
+						className=""
+					>
+						<MenuItem value={0}>
+							<em>All</em>
+						</MenuItem>
+						<MenuItem value={1}>Joao</MenuItem>
+						<MenuItem value={2}>Mileide</MenuItem>
+						<MenuItem value={3}>Luan</MenuItem>
+					</Select>
+
+
+
+					<TextField
+						id="date"
+						label="Data Inicial"
+						type="date"
+						defaultValue={dateIni}
+						className={classes.textField}
+						InputLabelProps={{
+							shrink: true,
+						}}
+						onChange={(e) => setDateIni(e.target.value)}
+					/>
+
+					<TextField
+						id="date"
+						label="Data Final"
+						type="date"
+						defaultValue={dateFin}
+						className={classes.textField}
+						InputLabelProps={{
+							shrink: true,
+						}}
+						onChange={(e) => setDateFin(e.target.value)}
+					/>
+					<Button variant="outlined" color="primary" className={classes.button} onClick={() => buttonSearchByUserId()}>
+						Search
+					</Button>
+
+				</form>
 				<Grid container spacing={3}>
 					<Grid item xs={4}>
 						<Paper className={classes.customFixedHeightPaper} style={{ overflow: "hidden" }}>
@@ -78,7 +171,7 @@ const Dashboard = () => {
 							</Typography>
 							<Grid item>
 								<Typography component="h1" variant="h4">
-									{GetTickets("open", "true", "false")}
+									{openTickets}
 								</Typography>
 							</Grid>
 						</Paper>
@@ -90,7 +183,7 @@ const Dashboard = () => {
 							</Typography>
 							<Grid item>
 								<Typography component="h1" variant="h4">
-									{GetTickets("pending", "true", "false")}
+									{pendingTickets}
 								</Typography>
 							</Grid>
 						</Paper>
@@ -102,7 +195,7 @@ const Dashboard = () => {
 							</Typography>
 							<Grid item>
 								<Typography component="h1" variant="h4">
-									{GetTickets("closed", "true", "false")}
+									{closedTickets}
 								</Typography>
 							</Grid>
 						</Paper>
